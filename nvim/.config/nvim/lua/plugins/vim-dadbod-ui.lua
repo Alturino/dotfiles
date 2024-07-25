8e696e78b1b13e15e853baeb73303a11d2b9a341
Ricky Alturino <alturino001@gmail.com>
2024-07-25 15:26:27 +0700

    add bigfile.nvim plugins, move dart-vim-plugin, and vim-dadbod-ui config from options to plugins file

--- nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
+++ nvim/.config/nvim/lua/plugins/vim-dadbod-ui.lua
@@ -14,7 +14,8 @@ return {
   init = function()
     -- Your DBUI configuration
     vim.g.db_ui_execute_on_save = 0
-    vim.g.db_ui_save_location = "~/.local/share/db_ui"
+    vim.g.db_ui_save_location = "./"
+    vim.g.db_ui_show_database_icon = 1
     vim.g.db_ui_tmp_query_location = vim.g.db_ui_save_location .. "/queries/"
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_use_nvim_notify = 1
