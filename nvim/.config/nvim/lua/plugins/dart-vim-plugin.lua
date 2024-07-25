d4636742843b1b6e01195a5e2ae8eb7897ef6abf
Ricky Alturino <alturino001@gmail.com>
2024-07-25 15:26:27 +0700

    add bigfile.nvim plugins, move dart-vim-plugin, and vim-dadbod-ui config from options to plugins file

--- nvim/.config/nvim/lua/plugins/dart-vim-plugin.lua
+++ nvim/.config/nvim/lua/plugins/dart-vim-plugin.lua
@@ -3,5 +3,11 @@ return {
     "dart-lang/dart-vim-plugin",
     ft = { "dart" },
     event = "BufEnter *.dart",
+    init = function()
+      vim.g.dart_format_on_save = true
+      vim.g.dart_html_in_string = true
+      vim.g.dart_style_guid = 2
+      vim.g.dart_trailing_comma_indent = true
+    end,
   },
 }
