4a92ab2d4ad65a765e1d04f655aba5997b84ae25
Ricky Alturino <alturino001@gmail.com>
2024-08-30 17:46:43 +0700

    chore(nvim): add keymaps and disable format on save for nvim

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -8,27 +8,27 @@ return {
       local ca = null_ls.builtins.code_actions
       local f = null_ls.builtins.formatting
 
-      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      -- local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
       return {
-        debug = true,
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                vim.lsp.buf.format({
-                  bufnr = bufnr,
-                  filter = function(c)
-                    return c.name == "null-ls"
-                  end,
-                })
-              end,
-            })
-          end
-        end,
+        debug = false,
+        -- on_attach = function(client, bufnr)
+        --   if client.supports_method("textDocument/formatting") then
+        --     vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+        --     vim.api.nvim_create_autocmd("BufWritePre", {
+        --       group = augroup,
+        --       buffer = bufnr,
+        --       callback = function()
+        --         vim.lsp.buf.format({
+        --           bufnr = bufnr,
+        --           filter = function(c)
+        --             return c.name == "null-ls"
+        --           end,
+        --         })
+        --       end,
+        --     })
+        --   end
+        -- end,
         sources = {
           ca.gitsigns,
           ca.gomodifytags,
@@ -41,6 +41,7 @@ return {
           d.golangci_lint,
           d.hadolint,
           d.ktlint,
+          d.mypy,
           d.revive,
           d.rubocop,
           d.sqlfluff,
