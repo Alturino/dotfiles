382282d4bfe5035959e215c4bd8426e7be800273
Ricky Alturino <alturino001@gmail.com>
2025-04-07 06:47:06 +0700

    chore(nvim): migrate from telescope to snack

--- nvim/.config/nvim/lua/plugins/snack.lua
+++ nvim/.config/nvim/lua/plugins/snack.lua
@@ -1,11 +1,40 @@
+local exclude = {
+  ".git",
+  "node_modules",
+  "vendor",
+  ".yarn/cache",
+  ".yarn/releases",
+  ".pnpm-store",
+  "*.vim",
+}
 return {
   {
     "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
     opts = {
-      dashboard = { enabled = false },
+      picker = {
+        sources = {
+          files = {
+            exclude = exclude,
+          },
+        },
+      },
+      explorer = {},
       scroll = { enabled = false },
+      dashboard = { enabled = false },
+    },
+    keys = {
+      -- Top Pickers & Explorer
+      -- stylua: ignore start
+      { "<leader>fw", function() Snacks.picker.grep({ exclude = exclude, }) end, desc = "Grep", },
+      { "<leader>fh", function() Snacks.picker.help() end,                       desc = "Help Pages", },
+      { "<leader>fj", function() Snacks.picker.jumps() end,                      desc = "Jumps", },
+      { "<leader>fk", function() Snacks.picker.keymaps() end,                    desc = "Keymaps", },
+      { '<leader>f"', function() Snacks.picker.registers() end,                  desc = "Registers", },
+      { "<leader>fq", function() Snacks.picker.qflist() end,                     desc = "Quickfix List", },
+      { "<leader>fs", function() Snacks.picker.lsp_symbols() end,                desc = "LSP Symbols", },
+      { "<leader>fS", function() Snacks.picker.lsp_workspace_symbols() end,      desc = "LSP Workspace Symbols", },
+      { "<leader>ss", mode = { 'n', 'x', },                                      false },
+      { "<leader>fr", function() Snacks.picker.lsp_references() end,             nowait = true,                  desc = "References", },
     },
   },
 }
--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -2,20 +2,7 @@ return {
   "nvim-telescope/telescope.nvim",
   branch = "0.1.x",
   dependencies = { "Marskey/telescope-sg" },
-  keys = {
-    -- stylua: ignore start
-    { "<leader>ff", LazyVim.pick("find_files", { root = false, hidden = true, no_ignore = true }), desc = "Find Files (cwd)" },
-    { "<leader>fF", LazyVim.pick("find_files", { hidden = true, no_ignore = true }),               desc = "Find Files (Root Dir)" },
-    { "<leader>fw", "<cmd>Telescope live_grep<cr>",                                                desc = "Grep (cwd)" },
-    { "<leader>fh", "<cmd>Telescope help_tags<cr>",                                                desc = "Help Pages" },
-    { "<leader>fj", "<cmd>Telescope jumplist<cr>",                                                 desc = "Jump List" },
-    { "<leader>fk", "<cmd>Telescope keymaps<cr>",                                                  desc = "Keymaps" },
-    { '<leader>f"', "<cmd>Telescope registers<cr>",                                                desc = "Register" },
-    { "<leader>fq", "<cmd>Telescope quickfix<cr>",                                                 desc = "Quickfix" },
-    { "<leader>fs", function() require("telescope.builtin").lsp_dynamic_workspace_symbols() end,   desc = "Symbol", },
-    { "<leader>fS", function() require("telescope.builtin").lsp_workspace_symbols() end,           desc = "Symbol", },
-    { "<leader>fr", function() require("telescope.builtin").lsp_references() end,                  desc = "Find References", },
-  },
+  keys = {},
   opts = {
     pickers = {
       find_files = {
