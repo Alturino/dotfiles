3bc8d9d92c77ccb8d3d4419fb88b6cb15903796a
Ricky Alturino <alturino001@gmail.com>
2025-06-19 14:33:53 +0700

    fix: add ignored files to picker and exclude files in snack.picker

--- nvim/.config/nvim/lua/plugins/snack.lua
+++ nvim/.config/nvim/lua/plugins/snack.lua
@@ -1,11 +1,14 @@
 local exclude = {
-  ".git",
-  "node_modules",
-  "vendor",
-  ".yarn/cache",
-  ".yarn/releases",
-  ".pnpm-store",
-  "*.vim",
+  "**/.git/*",
+  "**/node_modules/*",
+  "**/vendor/*",
+  "**/dist/*",
+  "**/build/*",
+  "**/.yarn/cache/*",
+  "**/.yarn/releases/*",
+  "**/.pnpm-store/*",
+  "**/.vim",
+  "**/.package-lock.json",
 }
 return {
   {
@@ -14,6 +17,8 @@ return {
       picker = {
         sources = {
           files = {
+            hidden = true,
+            ignored = true,
             exclude = exclude,
           },
         },
@@ -25,16 +30,16 @@ return {
     keys = {
       -- Top Pickers & Explorer
       -- stylua: ignore start
-      { "<leader>fw", function() Snacks.picker.grep({ exclude = exclude, }) end, desc = "Grep", },
-      { "<leader>fh", function() Snacks.picker.help() end,                       desc = "Help Pages", },
-      { "<leader>fj", function() Snacks.picker.jumps() end,                      desc = "Jumps", },
-      { "<leader>fk", function() Snacks.picker.keymaps() end,                    desc = "Keymaps", },
-      { '<leader>f"', function() Snacks.picker.registers() end,                  desc = "Registers", },
-      { "<leader>fq", function() Snacks.picker.qflist() end,                     desc = "Quickfix List", },
-      { "<leader>fs", function() Snacks.picker.lsp_symbols() end,                desc = "LSP Symbols", },
-      { "<leader>fS", function() Snacks.picker.lsp_workspace_symbols() end,      desc = "LSP Workspace Symbols", },
-      { "<leader>ss", mode = { 'n', 'x', },                                      false },
-      { "<leader>fr", function() Snacks.picker.lsp_references() end,             nowait = true,                  desc = "References", },
+      { "<leader>fw", function() Snacks.picker.grep({ hidden = true, ignored = false, exclude = exclude, }) end, desc = "Grep", },
+      { "<leader>fh", function() Snacks.picker.help() end,                                                       desc = "Help Pages", },
+      { "<leader>fj", function() Snacks.picker.jumps() end,                                                      desc = "Jumps", },
+      { "<leader>fk", function() Snacks.picker.keymaps() end,                                                    desc = "Keymaps", },
+      { '<leader>f"', function() Snacks.picker.registers() end,                                                  desc = "Registers", },
+      { "<leader>fq", function() Snacks.picker.qflist() end,                                                     desc = "Quickfix List", },
+      { "<leader>fs", function() Snacks.picker.lsp_symbols() end,                                                desc = "LSP Symbols", },
+      { "<leader>fS", function() Snacks.picker.lsp_workspace_symbols() end,                                      desc = "LSP Workspace Symbols", },
+      { "<leader>ss", mode = { 'n', 'x', },                                                                      false },
+      { "<leader>fr", function() Snacks.picker.lsp_references() end,                                             nowait = true,                  desc = "References", },
     },
   },
 }
