1b19fb762de2ac700289459b4eaf7086057f36f2
Ricky Alturino <alturino001@gmail.com>
2025-09-17 14:05:41 +0700

    chore: change event obsession to verylazy

--- nvim/.config/nvim/lua/plugins/obsession.lua
+++ nvim/.config/nvim/lua/plugins/obsession.lua
@@ -1,6 +1,7 @@
 return {
   {
     "tpope/vim-obsession",
+    event = "VeryLazy",
     cmd = { "Obsess" },
     keys = {
       { "<leader>os", "<cmd>Obsess<cr>", desc = "Save Session", nowait = true },
