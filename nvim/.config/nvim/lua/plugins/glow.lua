8361c904291044f54f6a3f4bdee53bb50f47e700
Ricky Alturino <alturino001@gmail.com>
2026-07-17 08:42:27 +0700

    disable glow.nvim

--- nvim/.config/nvim/lua/plugins/glow.lua
+++ nvim/.config/nvim/lua/plugins/glow.lua
@@ -1,19 +1,19 @@
 return {
-  {
-    "ellisonleao/glow.nvim",
-    event = "BufEnter *.md",
-    opts = function()
-      local glow = require("mason-registry").get_package("glow"):get_install_path() .. "/glow"
-      return {
-        glow_path = glow,
-        install_path = glow,
-        style = "dark",
-        width = 120,
-      }
-    end,
-    config = function(_, opts)
-      require("glow").setup(opts)
-    end,
-    cmd = "Glow",
-  },
+  -- {
+  --   "ellisonleao/glow.nvim",
+  --   event = "BufEnter *.md",
+  --   opts = function()
+  --     local glow = require("mason-registry").get_package("glow"):get_install_path() .. "/glow"
+  --     return {
+  --       glow_path = glow,
+  --       install_path = glow,
+  --       style = "dark",
+  --       width = 120,
+  --     }
+  --   end,
+  --   config = function(_, opts)
+  --     require("glow").setup(opts)
+  --   end,
+  --   cmd = "Glow",
+  -- },
 }
